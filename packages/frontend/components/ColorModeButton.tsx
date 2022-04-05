22055a060c51ab4739f305474c691efa5985a909
HKerStanley <stanleychiu@protonmail.com>
2022-04-06 00:05:59 +0800

    Add template hardhat project

--- packages/frontend/components/ColorModeButton.tsx
+++ packages/frontend/components/ColorModeButton.tsx
@@ -1,12 +1,14 @@
-import { Button, useColorMode } from "@chakra-ui/react";
+import { IconButton, useColorMode } from "@chakra-ui/react";
 import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 const ColorModeButton = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Button onClick={toggleColorMode}>
-      {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-    </Button>
+    <IconButton
+      onClick={toggleColorMode}
+      aria-label="Toggle Color Mode"
+      icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+    />
   );
 };
 
--- packages/frontend/components/Navbar.tsx
+++ packages/frontend/components/Navbar.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Divider, HStack, Text } from "@chakra-ui/react";
+import { Box, Divider, HStack, Icon, Heading, Link } from "@chakra-ui/react";
+import { GoMarkGithub } from "react-icons/go";
 import ColorModeButton from "./ColorModeButton";
 import WalletConnectButton from "./WalletConnectButton";
 
@@ -6,9 +7,15 @@ const Navbar = () => {
   return (
     <nav>
       <HStack justify="center" padding={"0.5rem 1rem"}>
-        <Text fontSize="2xl">ZKAutoChess</Text>
+        <Heading fontSize="2xl">ZKAutoChess</Heading>
         <Box width={"80%"}></Box>
-
+        <Link
+          href={"https://github.com/HKerStanley/zkautochess"}
+          isExternal
+          alignItems={"center"}
+        >
+          <Icon as={GoMarkGithub} w={8} h={8} pos={"relative"} bottom={-0.5} />
+        </Link>
         <ColorModeButton />
         <WalletConnectButton />
       </HStack>
