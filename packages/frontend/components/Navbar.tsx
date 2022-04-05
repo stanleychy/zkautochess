141f56d7e131526ba94738f7782c12ba1d32441d
HKerStanley <stanleychiu@protonmail.com>
2022-04-05 16:13:41 +0800

    Add ColorModeButton

--- packages/frontend/components/Navbar.tsx
+++ packages/frontend/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Divider, HStack, Text } from "@chakra-ui/react";
+import ColorModeButton from "./ColorModeButton";
 import WalletConnectButton from "./WalletConnectButton";
 
 const Navbar = () => {
@@ -7,6 +8,8 @@ const Navbar = () => {
       <HStack justify="center" padding={"0.5rem 1rem"}>
         <Text fontSize="2xl">ZKAutoChess</Text>
         <Box width={"80%"}></Box>
+
+        <ColorModeButton />
         <WalletConnectButton />
       </HStack>
       <Divider />
